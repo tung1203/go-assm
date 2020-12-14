26869a005c87f890ad81384d6908d3b7e3dfbec2
domanhdat <tung120320@gmail.com>
2020-12-15 00:55:54 +0700

    push

--- dal/DbUtil.go
+++ dal/DbUtil.go
@@ -15,13 +15,16 @@ var db *gorm.DB
 
 //InitializeMySQL to OrderDB
 func InitializeMySQL() {
-	dsn := os.Getenv("DATABASE_URL")
-	println(dsn)
+	dsn1 := os.Getenv("DATABASE_URL")
+
 	// dBConnection, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	// if err != nil {
 	// 	log.Fatalf("Error opening database: %q", err)
 	// }
 	// dsn := "root:tung1203@tcp(127.0.0.1:3306)/quizz?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "mysql://bc3e4c2f9def1b:c45e50e7@us-cdbr-east-02.cleardb.com/heroku_5598edf86a828a0?reconnect=true"
+	println(dsn)
+	println(dsn1)
 	dBConnection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
